27e92c68f659cfb5c0ce16c4ff387c1282e4b643
yut-kt <32399978+yut-kt@users.noreply.github.com>
2021-08-14 17:03:36 +0900

    V0.1.3 (#17)
    * Feature/refactor (#14)
    
    * fix: package comment prefix
    
    * fix: add struct comment
    
    * fix: add constructor comment
    
    * fix: add ReadData func comment
    
    * update: change main go file name
    
    * update: Larger examples
    
    * update: change generated sample wave member package name
    
    * update: sample package name
    
    * update: validate samplingNumber
    
    * update: test wave equal check
    
    * update: generate same seek io
    
    * update: Add minimal test
    
    * Feature/byte read control by seek offset (#15)
    
    * update: io.Reader to io.ReadSeeker
    
    * update: changed the file io of dataChunk to readSeeker for the time being
    
    * update: byte read control by seek offset
    
    * Feature/version update (#16)
    
    * add: ci linter
    
    * update: init test wave by enum
    
    * add: benchmark test
    
    * add: version metrics
    
    * update: readme badge
    
    * fix: S1005: unnecessary assignment to the blank identifier

---
+++ gowave_benchmark_test.go
@@ -0,0 +1,44 @@
+package gowave
+
+import (
+	"os"
+	"testing"
+
+	"github.com/yut-kt/gowave/internal/samples/wave_member_structs"
+)
+
+var waveFiles = map[string]*Wave{
+	"internal/samples/waves/A.wav": {
+		riffChunk: wave_member_structs.GetRiffChunkA(),
+		fmtChunk:  wave_member_structs.GetFmtChunkA(),
+		dataChunk: wave_member_structs.GetDataChunkA(),
+	},
+}
+
+func BenchmarkNew(b *testing.B) {
+	for waveFile := range waveFiles {
+		f, err := os.Open(waveFile)
+		if err != nil {
+			b.Errorf("os.Open(%v) error = %v", waveFile, err)
+		}
+		if _, err := New(f); err != nil {
+			b.Errorf("New(f) error = %v", err)
+		}
+	}
+}
+
+func BenchmarkWave_ReadSamples(b *testing.B) {
+	for _, wave := range waveFiles {
+		if _, err := wave.ReadSamples(); err != nil {
+			b.Errorf("wave.ReadSamples() error = %v", err)
+		}
+	}
+}
+
+func BenchmarkWave_ReadNSamples(b *testing.B) {
+	for _, wave := range waveFiles {
+		if _, err := wave.ReadNSamples(1000); err != nil {
+			b.Errorf("wave.ReadNSamples(1000) error = %v", err)
+		}
+	}
+}
--- wave.go
+++ wave.go
@@ -1,29 +1,30 @@
-// The package gowave provides support for reading WAV files.
+// Package gowave provides support for reading WAV files.
 package gowave
 
 import (
+	"errors"
 	"io"
 
 	"github.com/yut-kt/gowave/internal/chunk"
 )
 
+// Wave is a structure that handles wav files.
 type Wave struct {
 	riffChunk *chunk.RiffChunk
 	fmtChunk  *chunk.FmtChunk
 	dataChunk *chunk.DataChunk
-	file      io.Reader
 }
 
 // New is a function to construct Wave struct.
-func New(file io.Reader) (*Wave, error) {
-	wave := &Wave{file: file}
+func New(file io.ReadSeeker) (*Wave, error) {
+	wave := new(Wave)
 	if err := wave.chunkRead(file); err != nil {
 		return nil, err
 	}
 	return wave, nil
 }
 
-func (wave *Wave) chunkRead(file io.Reader) error {
+func (wave *Wave) chunkRead(file io.ReadSeeker) error {
 	riffChunk, err := chunk.NewRiffChunk(file)
 	if err != nil {
 		return err
@@ -48,7 +49,7 @@ func (wave *Wave) chunkRead(file io.Reader) error {
 // ReadSamples is a function to read all samples wave data.
 func (wave *Wave) ReadSamples() (interface{}, error) {
 	const fullRead = -1
-	data, err := wave.dataChunk.ReadData(wave.file, wave.fmtChunk.BitsPerSample, fullRead)
+	data, err := wave.dataChunk.ReadData(wave.fmtChunk.BitsPerSample, fullRead)
 	if err != nil {
 		return nil, err
 	}
@@ -56,8 +57,11 @@ func (wave *Wave) ReadSamples() (interface{}, error) {
 }
 
 // ReadNSamples is a function to read N samples wave data.
-func (wave *Wave) ReadNSamples(samplingNum int) (interface{}, error) {
-	data, err := wave.dataChunk.ReadData(wave.file, wave.fmtChunk.BitsPerSample, samplingNum)
+func (wave *Wave) ReadNSamples(samplingNum int64) (interface{}, error) {
+	if samplingNum < 1 {
+		return nil, errors.New("samplingNum is only natural number")
+	}
+	data, err := wave.dataChunk.ReadData(wave.fmtChunk.BitsPerSample, samplingNum)
 	if err != nil {
 		return nil, err
 	}
